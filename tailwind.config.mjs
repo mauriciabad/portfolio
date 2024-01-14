6c5b17f8f37f529d024516f4fa88f188f897cc3c
maui <hello@mauriciabad.com>
2024-01-14 20:37:54 +0100

    Add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+const sansFonts = [
+  "Inter Variable",
+  "InterVariable",
+  "Inter",
+  ...defaultTheme.fontFamily.sans,
+]
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: sansFonts,
+        title: ["Fraunces Variable", "Fraunces", ...sansFonts],
+      },
+    },
   },
   plugins: ["@tailwindcss/typography"],
 }
